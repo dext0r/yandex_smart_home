34b6e0db147e00c77d508a685fdc05d586b21b23
Gleb <glebsterx@gmail.com>
2021-02-19 08:26:38 +0300

    YandexSkill connection & Event listener

--- custom_components/yandex_smart_home/__init__.py
+++ custom_components/yandex_smart_home/__init__.py
@@ -1,12 +1,13 @@
 """Support for Actions on Yandex Smart Home."""
+import asyncio
 import logging
 from typing import Dict, Any
 
 import voluptuous as vol
 
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, Event
 
-from homeassistant.const import CONF_NAME
+from homeassistant.const import CONF_NAME, CONF_USERNAME, CONF_PASSWORD
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers import entityfilter
 
@@ -14,8 +15,10 @@ from .const import (
     DOMAIN, CONF_ENTITY_CONFIG, CONF_FILTER, CONF_ROOM, CONF_TYPE,
     CONF_ENTITY_PROPERTIES, CONF_ENTITY_PROPERTY_ENTITY, CONF_ENTITY_PROPERTY_ATTRIBUTE, CONF_ENTITY_PROPERTY_TYPE,
     CONF_CHANNEL_SET_VIA_MEDIA_CONTENT_ID, CONF_RELATIVE_VOLUME_ONLY, CONF_ENTITY_RANGE, CONF_ENTITY_RANGE_MAX, 
-    CONF_ENTITY_RANGE_MIN, CONF_ENTITY_RANGE_PRECISION, CONF_ENTITY_MODE_MAP)
+    CONF_ENTITY_RANGE_MIN, CONF_ENTITY_RANGE_PRECISION, CONF_ENTITY_MODE_MAP,
+	CONF_SKILL, CONF_SKILL_NAME, CONF_SKILL_USER, CONF_SKILL_OAUTH_TOKEN)
 from .http import async_register_http
+from .skill import YandexSkill
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -42,8 +45,17 @@ ENTITY_SCHEMA = vol.Schema({
     vol.Optional(CONF_ENTITY_MODE_MAP, default={}): {cv.string: {cv.string: [cv.string]}},
 })
 
+SKILL_SCHEMA = vol.Schema({
+    vol.Optional(CONF_USERNAME): cv.string,
+    vol.Optional(CONF_PASSWORD): cv.string,
+    vol.Optional(CONF_SKILL_NAME): cv.string,
+    vol.Optional(CONF_SKILL_USER): cv.string,
+    vol.Optional(CONF_SKILL_OAUTH_TOKEN): cv.string,
+}, extra=vol.PREVENT_EXTRA)
+
 YANDEX_SMART_HOME_SCHEMA = vol.All(
     vol.Schema({
+        vol.Optional(CONF_SKILL, default={}): SKILL_SCHEMA,
         vol.Optional(CONF_FILTER, default={}): entityfilter.FILTER_SCHEMA,
         vol.Optional(CONF_ENTITY_CONFIG, default={}): {cv.entity_id: ENTITY_SCHEMA},
     }, extra=vol.PREVENT_EXTRA))
@@ -57,5 +69,15 @@ async def async_setup(hass: HomeAssistant, yaml_config: Dict[str, Any]):
     """Activate Yandex Smart Home component."""
     config = yaml_config.get(DOMAIN, {})
     async_register_http(hass, config)
+
+    skill = YandexSkill(hass, config)
+
+    async def listener(event: Event):
+        try:
+            await skill.async_event_handler(event)
+        except Exception:
+            _LOGGER.exception("Event handler error")
+
+    hass.bus.async_listen('state_changed', listener)
 
     return True
