59398a097d9963770732a184fcebec3a8c08e900
Dmitry Krivomazov <supermegadi@gmail.com>
2021-06-24 00:03:16 +0700

    Revert "add mode swing"
    This reverts commit 63618f459ccbd6e91176d17254159e25b877c00c.

--- custom_components/yandex_smart_home/capability.py
+++ custom_components/yandex_smart_home/capability.py
@@ -495,75 +495,6 @@ class ThermostatCapability(_ModeCapability):
 
 
 @register_capability
-class SwingCapability(_ModeCapability):
-    """Swing functionality"""
-
-    instance = 'swing'
-
-    modes_map = {
-        climate.const.SWING_VERTICAL: 'vertical',
-        climate.const.SWING_HORIZONTAL: 'horizontal',
-        climate.const.SWING_OFF: 'stationary',
-        climate.const.SWING_BOTH: 'auto'
-    }
-
-    @staticmethod
-    def supported(domain, features, entity_config, attributes):
-        """Test if state is supported."""
-        if domain == climate.DOMAIN:
-            return features & climate.SUPPORT_SWING_MODE
-        return False
-
-    def parameters(self):
-        """Return parameters for a devices request."""
-        operation_list = self.state.attributes.get(climate.ATTR_SWING_MODES)
-        modes = []
-        added = []
-
-        for ha_value in operation_list:
-            value = self.get_yandex_mode_by_ha_mode(ha_value)
-            if value is not None and value not in added:
-                modes.append({'value': value})
-                added.append(value)
-
-        return {
-            'instance': self.instance,
-            'modes': modes,
-            'ordered': False
-        }
-
-    def get_value(self):
-        """Return the state value of this capability for this entity."""
-        operation = self.state.attributes.get(climate.ATTR_SWING_MODE)
-
-        if operation is not None and operation in self.modes_map:
-            return self.modes_map[operation]
-
-        if operation is not None:
-            yandex_value = self.get_yandex_mode_by_ha_mode(operation)
-            if yandex_value is not None:
-                return yandex_value
-
-        modes = self.parameters()['modes']
-
-        return modes[0]['value'] if len(modes) > 0 else 'auto'
-
-    async def set_state(self, data, state):
-        """Set device state."""
-        value = self.get_ha_mode_by_yandex_mode(state['value'], self.state.attributes.get(climate.ATTR_SWING_MODES))
-
-        if value is None:
-            raise SmartHomeError(ERR_INVALID_VALUE, "Unacceptable value")
-
-        await self.hass.services.async_call(
-            climate.DOMAIN,
-            climate.SERVICE_SET_SWING_MODE, {
-                ATTR_ENTITY_ID: self.state.entity_id,
-                climate.ATTR_SWING_MODE: value
-            }, blocking=True, context=data.context)
-
-
-@register_capability
 class ProgramCapability(_ModeCapability):
     """Program functionality"""
 
@@ -853,7 +784,6 @@ class _RangeCapability(_Capability):
 
     type = CAPABILITIES_RANGE
 
-
 @register_capability
 class CoverLevelCapability(_RangeCapability):
     """Set cover level"""
@@ -908,7 +838,6 @@ class CoverLevelCapability(_RangeCapability):
                 ATTR_ENTITY_ID: self.state.entity_id,
                 attr: value
             }, blocking=True, context=data.context)
-
 
 @register_capability
 class TemperatureCapability(_RangeCapability):
@@ -1394,7 +1323,6 @@ class RgbCapability(_ColorSettingCapability):
                 light.ATTR_RGB_COLOR: (red, green, blue)
             }, blocking=True, context=data.context)
 
-
 @register_capability
 class TemperatureKCapability(_ColorSettingCapability):
     """Color temperature functionality."""
@@ -1424,7 +1352,6 @@ class TemperatureKCapability(_ColorSettingCapability):
                 ATTR_ENTITY_ID: self.state.entity_id,
                 light.ATTR_KELVIN: state['value']
             }, blocking=True, context=data.context)
-
 
 @register_capability
 class ColorSceneCapability(_ColorSettingCapability):
