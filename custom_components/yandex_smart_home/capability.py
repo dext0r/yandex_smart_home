a7b06c31c504a83a01c8dc6021758d8e35e8225b
artem.sedykh <anavrin72@gmail.com>
2021-06-24 20:11:43 +0300

    add mode thermostat, swing

--- custom_components/yandex_smart_home/capability.py
+++ custom_components/yandex_smart_home/capability.py
@@ -424,7 +424,7 @@ class ThermostatCapability(_ModeCapability):
 
     instance = 'thermostat'
 
-    climate_map = {
+    modes_map = {
         climate.const.HVAC_MODE_HEAT: 'heat',
         climate.const.HVAC_MODE_COOL: 'cool',
         climate.const.HVAC_MODE_AUTO: 'auto',
@@ -438,7 +438,7 @@ class ThermostatCapability(_ModeCapability):
         if domain == climate.DOMAIN:
             operation_list = attributes.get(climate.ATTR_HVAC_MODES)
             for operation in operation_list:
-                if operation in ThermostatCapability.climate_map:
+                if operation in ThermostatCapability.modes_map:
                     return True
         return False
 
@@ -446,9 +446,13 @@ class ThermostatCapability(_ModeCapability):
         """Return parameters for a devices request."""
         operation_list = self.state.attributes.get(climate.ATTR_HVAC_MODES)
         modes = []
-        for operation in operation_list:
-            if operation in self.climate_map:
-                modes.append({'value': self.climate_map[operation]})
+        added = []
+
+        for ha_value in operation_list:
+            value = self.get_yandex_mode_by_ha_mode(ha_value)
+            if value is not None and value not in added:
+                modes.append({'value': value})
+                added.append(value)
 
         return {
             'instance': self.instance,
@@ -458,30 +462,23 @@ class ThermostatCapability(_ModeCapability):
 
     def get_value(self):
         """Return the state value of this capability for this entity."""
-        operation = self.state.attributes.get(climate.ATTR_HVAC_MODE)
-
-        if operation is not None and operation in self.climate_map:
-            return self.climate_map[operation]
-        
-        # Try to take current mode from device state
         operation = self.state.state
-        if operation is not None and operation in self.climate_map:
-            return self.climate_map[operation]
 
-        # Return first value if current one is not acceptable
-        for operation in self.state.attributes.get(climate.ATTR_HVAC_MODES):
-            if operation in self.climate_map:
-                return self.climate_map[operation]
+        if operation is not None and operation in self.modes_map:
+            return self.modes_map[operation]
 
-        return 'auto'
+        if operation is not None:
+            yandex_value = self.get_yandex_mode_by_ha_mode(operation)
+            if yandex_value is not None:
+                return yandex_value
+
+        modes = self.parameters()['modes']
+
+        return modes[0]['value'] if len(modes) > 0 else 'auto'
 
     async def set_state(self, data, state):
         """Set device state."""
-        value = None
-        for climate_value, yandex_value in self.climate_map.items():
-            if yandex_value == state['value']:
-                value = climate_value
-                break
+        value = self.get_ha_mode_by_yandex_mode(state['value'], self.state.attributes.get(climate.ATTR_HVAC_MODES))
 
         if value is None:
             raise SmartHomeError(ERR_INVALID_VALUE, "Unacceptable value")
@@ -491,6 +488,75 @@ class ThermostatCapability(_ModeCapability):
             climate.SERVICE_SET_HVAC_MODE, {
                 ATTR_ENTITY_ID: self.state.entity_id,
                 climate.ATTR_HVAC_MODE: value
+            }, blocking=True, context=data.context)
+
+
+@register_capability
+class SwingCapability(_ModeCapability):
+    """Swing functionality"""
+
+    instance = 'swing'
+
+    modes_map = {
+        climate.const.SWING_VERTICAL: 'vertical',
+        climate.const.SWING_HORIZONTAL: 'horizontal',
+        climate.const.SWING_OFF: 'stationary',
+        climate.const.SWING_BOTH: 'auto'
+    }
+
+    @staticmethod
+    def supported(domain, features, entity_config, attributes):
+        """Test if state is supported."""
+        if domain == climate.DOMAIN:
+            return features & climate.SUPPORT_SWING_MODE
+        return False
+
+    def parameters(self):
+        """Return parameters for a devices request."""
+        operation_list = self.state.attributes.get(climate.ATTR_SWING_MODES)
+        modes = []
+        added = []
+
+        for ha_value in operation_list:
+            value = self.get_yandex_mode_by_ha_mode(ha_value)
+            if value is not None and value not in added:
+                modes.append({'value': value})
+                added.append(value)
+
+        return {
+            'instance': self.instance,
+            'modes': modes,
+            'ordered': False
+        }
+
+    def get_value(self):
+        """Return the state value of this capability for this entity."""
+        operation = self.state.attributes.get(climate.ATTR_SWING_MODE)
+
+        if operation is not None and operation in self.modes_map:
+            return self.modes_map[operation]
+
+        if operation is not None:
+            yandex_value = self.get_yandex_mode_by_ha_mode(operation)
+            if yandex_value is not None:
+                return yandex_value
+
+        modes = self.parameters()['modes']
+
+        return modes[0]['value'] if len(modes) > 0 else 'auto'
+
+    async def set_state(self, data, state):
+        """Set device state."""
+        value = self.get_ha_mode_by_yandex_mode(state['value'], self.state.attributes.get(climate.ATTR_SWING_MODES))
+
+        if value is None:
+            raise SmartHomeError(ERR_INVALID_VALUE, "Unacceptable value")
+
+        await self.hass.services.async_call(
+            climate.DOMAIN,
+            climate.SERVICE_SET_SWING_MODE, {
+                ATTR_ENTITY_ID: self.state.entity_id,
+                climate.ATTR_SWING_MODE: value
             }, blocking=True, context=data.context)
 
 
