2c3d4385c77a182ff9998a575b42b646a47721f9
Artem Sorokin <artem@sorokin.pp.ru>
2021-09-09 01:30:16 +0300

    Add tests for properties

---
+++ tests/test_prop_float.py
@@ -0,0 +1,404 @@
+from __future__ import annotations
+
+from homeassistant.components import air_quality, binary_sensor, climate, cover, fan, humidifier, light, sensor, switch
+from homeassistant.const import (
+    ATTR_BATTERY_LEVEL,
+    ATTR_DEVICE_CLASS,
+    ATTR_UNIT_OF_MEASUREMENT,
+    ATTR_VOLTAGE,
+    DEVICE_CLASS_BATTERY,
+    DEVICE_CLASS_CO2,
+    DEVICE_CLASS_CURRENT,
+    DEVICE_CLASS_HUMIDITY,
+    DEVICE_CLASS_ILLUMINANCE,
+    DEVICE_CLASS_POWER,
+    DEVICE_CLASS_PRESSURE,
+    DEVICE_CLASS_TEMPERATURE,
+    DEVICE_CLASS_VOLTAGE,
+    STATE_ON,
+    STATE_UNKNOWN,
+)
+from homeassistant.core import State
+import pytest
+
+from custom_components.yandex_smart_home import const
+from custom_components.yandex_smart_home.error import SmartHomeError
+from custom_components.yandex_smart_home.prop import PROPERTY_EVENT, PROPERTY_FLOAT, _FloatProperty
+
+from . import BASIC_CONFIG, MockConfig
+from .test_prop import assert_no_properties, get_exact_one_property
+
+
+class MockFloatProperty(_FloatProperty):
+    @staticmethod
+    def supported(*args, **kwargs):
+        return True
+
+    def get_value(self):
+        pass
+
+
+async def test_property_float(hass):
+    prop = MockFloatProperty(hass, BASIC_CONFIG, State('binary_sensor.test', STATE_ON))
+    assert prop.convert_value(1, 'foo') == 1
+
+
+@pytest.mark.parametrize('domain,device_class,attribute,supported', [
+    (sensor.DOMAIN, DEVICE_CLASS_HUMIDITY, None, True),
+    (air_quality.DOMAIN, None, climate.ATTR_HUMIDITY, True),
+    (air_quality.DOMAIN, None, None, False),
+    (climate.DOMAIN, None, climate.ATTR_CURRENT_HUMIDITY, True),
+    (climate.DOMAIN, None, None, False),
+    (fan.DOMAIN, None, climate.ATTR_CURRENT_HUMIDITY, True),
+    (fan.DOMAIN, None, None, False),
+    (humidifier.DOMAIN, None, climate.ATTR_CURRENT_HUMIDITY, True),
+    (humidifier.DOMAIN, None, None, False),
+])
+async def test_property_float_humidity(hass, domain, device_class, attribute, supported):
+    attributes = {}
+    value = STATE_ON
+
+    if attribute is None:
+        value = 69
+    else:
+        attributes[attribute] = 69
+
+    if device_class:
+        attributes[ATTR_DEVICE_CLASS] = device_class
+
+    state = State(f'{domain}.test', value, attributes)
+    if supported:
+        prop = get_exact_one_property(hass, BASIC_CONFIG, state, PROPERTY_FLOAT, const.PROPERTY_TYPE_HUMIDITY)
+    else:
+        assert_no_properties(hass, BASIC_CONFIG, state, PROPERTY_FLOAT, const.PROPERTY_TYPE_HUMIDITY)
+        return
+
+    assert prop.retrievable
+    assert prop.parameters() == {'instance': 'humidity', 'unit': 'unit.percent'}
+
+    assert prop.get_value() == 69
+
+    if attribute is None:
+        value = STATE_UNKNOWN
+    else:
+        attributes[attribute] = None
+    prop.state = State(f'{domain}.test', value, attributes)
+    assert prop.get_value() is None
+
+
+@pytest.mark.parametrize('domain,device_class,attribute,supported', [
+    (sensor.DOMAIN, DEVICE_CLASS_TEMPERATURE, None, True),
+    (air_quality.DOMAIN, None, climate.ATTR_TEMPERATURE, True),
+    (air_quality.DOMAIN, None, None, False),
+    (climate.DOMAIN, None, climate.ATTR_CURRENT_TEMPERATURE, True),
+    (climate.DOMAIN, None, None, False),
+    (fan.DOMAIN, None, climate.ATTR_CURRENT_TEMPERATURE, True),
+    (fan.DOMAIN, None, None, False),
+    (humidifier.DOMAIN, None, climate.ATTR_CURRENT_TEMPERATURE, True),
+    (humidifier.DOMAIN, None, None, False),
+])
+async def test_property_float_temperature(hass, domain, device_class, attribute, supported):
+    attributes = {}
+    value = STATE_ON
+
+    if attribute is None:
+        value = 34
+    else:
+        attributes[attribute] = 34
+
+    if device_class:
+        attributes[ATTR_DEVICE_CLASS] = device_class
+
+    state = State(f'{domain}.test', value, attributes)
+    if supported:
+        prop = get_exact_one_property(hass, BASIC_CONFIG, state, PROPERTY_FLOAT, const.PROPERTY_TYPE_TEMPERATURE)
+    else:
+        assert_no_properties(hass, BASIC_CONFIG, state, PROPERTY_FLOAT, const.PROPERTY_TYPE_TEMPERATURE)
+        return
+
+    assert prop.retrievable
+    assert prop.parameters() == {'instance': 'temperature', 'unit': 'unit.temperature.celsius'}
+
+    assert prop.get_value() == 34
+
+    if attribute is None:
+        value = STATE_UNKNOWN
+    else:
+        attributes[attribute] = None
+    prop.state = State(f'{domain}.test', value, attributes)
+    assert prop.get_value() is None
+
+
+@pytest.mark.parametrize('yandex_pressure_unit,v', [
+    (const.PRESSURE_UNIT_PASCAL, 98658.28),
+    (const.PRESSURE_UNIT_MMHG, 740),
+    (const.PRESSURE_UNIT_ATM, 0.97),
+    (const.PRESSURE_UNIT_BAR, 0.99),
+])
+def test_property_float_pressure(hass, yandex_pressure_unit, v):
+    config = MockConfig(
+        settings={
+            const.CONF_PRESSURE_UNIT: yandex_pressure_unit
+        }
+    )
+    state = State('sensor.test', '740', {
+        ATTR_DEVICE_CLASS: DEVICE_CLASS_PRESSURE
+    })
+
+    prop = get_exact_one_property(hass, config, state, PROPERTY_FLOAT, const.PROPERTY_TYPE_PRESSURE)
+    with pytest.raises(SmartHomeError):
+        prop.get_value()
+
+    state = State('sensor.test', '740', {
+        ATTR_DEVICE_CLASS: DEVICE_CLASS_PRESSURE,
+        ATTR_UNIT_OF_MEASUREMENT: const.PRESSURE_UNIT_MMHG
+    })
+    prop = get_exact_one_property(hass, config, state, PROPERTY_FLOAT, const.PROPERTY_TYPE_PRESSURE)
+    assert prop.retrievable
+    assert prop.parameters() == {
+        'instance': 'pressure',
+        'unit': const.PRESSURE_UNITS_TO_YANDEX_UNITS[yandex_pressure_unit]
+    }
+    assert prop.get_value() == v
+
+
+@pytest.mark.parametrize('domain,device_class,attribute,supported', [
+    (sensor.DOMAIN, DEVICE_CLASS_ILLUMINANCE, None, True),
+    (sensor.DOMAIN, None, None, False),
+    (light.DOMAIN, None, 'illuminance', True),
+    (light.DOMAIN, None, None, False),
+    (fan.DOMAIN, None, 'illuminance', True),
+    (fan.DOMAIN, None, None, False),
+])
+async def test_property_float_illuminance(hass, domain, device_class, attribute, supported):
+    attributes = {}
+    value = STATE_ON
+
+    if attribute is None:
+        value = 48
+    else:
+        attributes[attribute] = 48
+
+    if device_class:
+        attributes[ATTR_DEVICE_CLASS] = device_class
+
+    state = State(f'{domain}.test', value, attributes)
+    if supported:
+        prop = get_exact_one_property(hass, BASIC_CONFIG, state, PROPERTY_FLOAT, const.PROPERTY_TYPE_ILLUMINATION)
+    else:
+        assert_no_properties(hass, BASIC_CONFIG, state, PROPERTY_FLOAT, const.PROPERTY_TYPE_ILLUMINATION)
+        return
+
+    assert prop.retrievable
+    assert prop.parameters() == {'instance': 'illumination', 'unit': 'unit.illumination.lux'}
+    assert prop.get_value() == 48
+
+    if attribute is None:
+        value = STATE_UNKNOWN
+    else:
+        attributes[attribute] = None
+    prop.state = State(f'{domain}.test', value, attributes)
+    assert prop.get_value() is None
+
+
+@pytest.mark.parametrize('domain,attribute,supported', [
+    (fan.DOMAIN, 'water_level', True),
+    (humidifier.DOMAIN, 'water_level', True),
+])
+async def test_property_float_water_level(hass, domain, attribute, supported):
+    state = State(f'{domain}.test', STATE_ON, {attribute: '90'})
+    assert_no_properties(hass, BASIC_CONFIG, state, PROPERTY_EVENT, const.PROPERTY_TYPE_WATER_LEVEL)
+    if supported:
+        prop = get_exact_one_property(hass, BASIC_CONFIG, state, PROPERTY_FLOAT, const.PROPERTY_TYPE_WATER_LEVEL)
+    else:
+        assert_no_properties(hass, BASIC_CONFIG, state, PROPERTY_FLOAT, const.PROPERTY_TYPE_WATER_LEVEL)
+        return
+
+    assert prop.retrievable
+    assert prop.parameters() == {'instance': 'water_level', 'unit': 'unit.percent'}
+    assert prop.get_value() == 90
+
+    prop.state = State(f'{domain}.test', STATE_ON, {attribute: None})
+    assert prop.get_value() is None
+
+
+@pytest.mark.parametrize('domain,device_class,attribute,supported', [
+    (sensor.DOMAIN, DEVICE_CLASS_CO2, None, True),
+    (sensor.DOMAIN, None, None, False),
+    (air_quality.DOMAIN, None, air_quality.ATTR_CO2, True),
+    (air_quality.DOMAIN, None, None, False),
+    (fan.DOMAIN, None, air_quality.ATTR_CO2, True),
+    (fan.DOMAIN, None, None, False),
+])
+async def test_property_float_co2(hass, domain, device_class, attribute, supported):
+    attributes = {}
+    value = STATE_ON
+
+    if attribute is None:
+        value = 643
+    else:
+        attributes[attribute] = 643
+
+    if device_class:
+        attributes[ATTR_DEVICE_CLASS] = device_class
+
+    state = State(f'{domain}.test', value, attributes)
+    if supported:
+        prop = get_exact_one_property(hass, BASIC_CONFIG, state, PROPERTY_FLOAT, const.PROPERTY_TYPE_CO2_LEVEL)
+    else:
+        assert_no_properties(hass, BASIC_CONFIG, state, PROPERTY_FLOAT, const.PROPERTY_TYPE_CO2_LEVEL)
+        return
+
+    assert prop.retrievable
+    assert prop.parameters() == {'instance': 'co2_level', 'unit': 'unit.ppm'}
+    assert prop.get_value() == 643
+
+    if attribute is None:
+        value = STATE_UNKNOWN
+    else:
+        attributes[attribute] = None
+    prop.state = State(f'{domain}.test', value, attributes)
+    assert prop.get_value() is None
+
+
+@pytest.mark.parametrize('attribute,instance', [
+    (air_quality.ATTR_PM_0_1, const.PROPERTY_TYPE_PM1_DENSITY),
+    (air_quality.ATTR_PM_2_5, const.PROPERTY_TYPE_PM2_5_DENSITY),
+    (air_quality.ATTR_PM_10, const.PROPERTY_TYPE_PM10_DENSITY),
+])
+async def test_property_float_pm(hass, attribute, instance):
+    state = State('air_quality.test', STATE_ON, {attribute: 300})
+    prop = get_exact_one_property(hass, BASIC_CONFIG, state, PROPERTY_FLOAT, instance)
+
+    assert prop.retrievable
+    assert prop.parameters() == {'instance': instance, 'unit': 'unit.density.mcg_m3'}
+    assert prop.get_value() == 300
+
+    prop.state = State('air_quality.test', STATE_ON, {attribute: None})
+    assert prop.get_value() is None
+
+
+@pytest.mark.parametrize('unit,v', [
+    ('ppb', 134.89),
+    ('ppm', 134888.81),
+    ('µg/m³', 30),
+    ('mg/m³', 30000),
+    ('μg/ft³', 1059.44),
+    ('unsupported', 30),
+])
+async def test_property_float_tvoc(hass, unit, v):
+    state = State('air_quality.test', STATE_ON,)
+    assert_no_properties(hass, BASIC_CONFIG, state, PROPERTY_FLOAT, const.PROPERTY_TYPE_TVOC)
+
+    state = State('air_quality.test', STATE_ON, {
+        'total_volatile_organic_compounds': 30,
+        ATTR_UNIT_OF_MEASUREMENT: unit
+    })
+    prop = get_exact_one_property(hass, BASIC_CONFIG, state, PROPERTY_FLOAT, const.PROPERTY_TYPE_TVOC)
+
+    assert prop.retrievable
+    assert prop.parameters() == {'instance': const.PROPERTY_TYPE_TVOC, 'unit': 'unit.density.mcg_m3'}
+    assert prop.get_value() == v
+
+    prop.state = State('air_quality.test', STATE_ON, {'total_volatile_organic_compounds': None})
+    assert prop.get_value() is None
+
+
+@pytest.mark.parametrize('domain,device_class,attribute,instance,unit,supported', [
+    (binary_sensor.DOMAIN, DEVICE_CLASS_VOLTAGE, None, const.PROPERTY_TYPE_VOLTAGE, 'unit.volt', False),
+    (sensor.DOMAIN, DEVICE_CLASS_VOLTAGE, None, const.PROPERTY_TYPE_VOLTAGE, 'unit.volt', True),
+    (sensor.DOMAIN, None, None, const.PROPERTY_TYPE_VOLTAGE, 'unit.volt', False),
+    (switch.DOMAIN, None, ATTR_VOLTAGE, const.PROPERTY_TYPE_VOLTAGE, 'unit.volt', True),
+    (switch.DOMAIN, None, None, const.PROPERTY_TYPE_VOLTAGE, 'unit.volt', False),
+    (light.DOMAIN, None, ATTR_VOLTAGE, const.PROPERTY_TYPE_VOLTAGE, 'unit.volt', True),
+    (light.DOMAIN, None, None, const.PROPERTY_TYPE_VOLTAGE, 'unit.volt', False),
+
+    (binary_sensor.DOMAIN, DEVICE_CLASS_CURRENT, None, const.PROPERTY_TYPE_AMPERAGE, 'unit.ampere', False),
+    (sensor.DOMAIN, DEVICE_CLASS_CURRENT, None, const.PROPERTY_TYPE_AMPERAGE, 'unit.ampere', True),
+    (sensor.DOMAIN, None, None, const.PROPERTY_TYPE_AMPERAGE, 'unit.ampere', False),
+    (switch.DOMAIN, None, 'current', const.PROPERTY_TYPE_AMPERAGE, 'unit.ampere', True),
+    (switch.DOMAIN, None, None, const.PROPERTY_TYPE_AMPERAGE, 'unit.ampere', False),
+    (light.DOMAIN, None, 'current', const.PROPERTY_TYPE_AMPERAGE, 'unit.ampere', True),
+    (light.DOMAIN, None, None, const.PROPERTY_TYPE_AMPERAGE, 'unit.ampere', False),
+
+    (sensor.DOMAIN, DEVICE_CLASS_POWER, None, const.PROPERTY_TYPE_POWER, 'unit.watt', True),
+    (sensor.DOMAIN, None, None, const.PROPERTY_TYPE_POWER, 'unit.watt', False),
+    (switch.DOMAIN, None, 'power', const.PROPERTY_TYPE_POWER, 'unit.watt', True),
+    (switch.DOMAIN, None, 'load_power', const.PROPERTY_TYPE_POWER, 'unit.watt', True),
+    (switch.DOMAIN, None, None, const.PROPERTY_TYPE_POWER, 'unit.watt', False),
+])
+async def test_property_float_simple(hass, domain, device_class, attribute, instance: str, unit, supported):
+    attributes = {}
+    value = STATE_ON
+
+    if attribute is None:
+        value = 220
+    else:
+        attributes[attribute] = 220
+
+    if device_class:
+        attributes[ATTR_DEVICE_CLASS] = device_class
+
+    state = State(f'{domain}.test', value, attributes)
+    if supported:
+        prop = get_exact_one_property(hass, BASIC_CONFIG, state, PROPERTY_FLOAT, instance)
+    else:
+        assert_no_properties(hass, BASIC_CONFIG, state, PROPERTY_FLOAT, instance)
+        return
+
+    assert prop.retrievable
+    assert prop.parameters() == {'instance': instance, 'unit': unit}
+    assert prop.get_value() == 220
+
+    if attribute is None:
+        value = STATE_UNKNOWN
+    else:
+        attributes[attribute] = None
+    prop.state = State(f'{domain}.test', value, attributes)
+    assert prop.get_value() is None
+
+
+@pytest.mark.parametrize('domain', [switch.DOMAIN, sensor.DOMAIN, light.DOMAIN, cover.DOMAIN])
+async def test_property_float_battery_class(hass, domain):
+    state = State(f'{domain}.test', '50', {
+        ATTR_DEVICE_CLASS: DEVICE_CLASS_BATTERY
+    })
+    assert_no_properties(hass, BASIC_CONFIG, state, PROPERTY_EVENT, const.PROPERTY_TYPE_BATTERY_LEVEL)
+    prop = get_exact_one_property(hass, BASIC_CONFIG, state, PROPERTY_FLOAT, const.PROPERTY_TYPE_BATTERY_LEVEL)
+    assert prop.retrievable
+    assert prop.parameters() == {'instance': 'battery_level', 'unit': 'unit.percent'}
+    assert prop.get_value() == 50
+
+    prop.state = State(f'{domain}.test', STATE_UNKNOWN, {
+        ATTR_DEVICE_CLASS: DEVICE_CLASS_BATTERY
+    })
+    assert prop.get_value() is None
+
+    for s in ['low', 'charging']:
+        prop.state = State(f'{domain}.test', s, {
+            ATTR_DEVICE_CLASS: DEVICE_CLASS_BATTERY
+        })
+        assert prop.get_value() == 0
+
+
+@pytest.mark.parametrize('domain', [switch.DOMAIN, sensor.DOMAIN, light.DOMAIN, cover.DOMAIN])
+async def test_property_float_battery_attr(hass, domain):
+    state = State(f'{domain}.test', STATE_ON, {
+        ATTR_BATTERY_LEVEL: 50
+    })
+    assert_no_properties(hass, BASIC_CONFIG, state, PROPERTY_EVENT, const.PROPERTY_TYPE_BATTERY_LEVEL)
+    prop = get_exact_one_property(hass, BASIC_CONFIG, state, PROPERTY_FLOAT, const.PROPERTY_TYPE_BATTERY_LEVEL)
+    assert prop.retrievable
+    assert prop.parameters() == {'instance': 'battery_level', 'unit': 'unit.percent'}
+    assert prop.get_value() == 50
+
+    prop.state = State(f'{domain}.test', STATE_ON, {
+        ATTR_BATTERY_LEVEL: None
+    })
+    assert prop.get_value() is None
+
+    for s in ['low', 'charging']:
+        prop.state = State(f'{domain}.test', STATE_ON, {
+            ATTR_BATTERY_LEVEL: s
+        })
+        assert prop.get_value() == 0
